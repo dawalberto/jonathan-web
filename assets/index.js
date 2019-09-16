4689f891a40e2d568c91c7af5af0a2bd6ad10c74
Alberto Garcia <albertoinformatica97@gmail.com>
2019-09-17 00:38:41 +0200

    Menu responsive casi terminado

---
+++ assets/index.js
@@ -0,0 +1,4 @@
+function toogleNav() {
+    let navMenu = document.getElementById('navMenu');
+    navMenu.style.display === 'block' ? navMenu.style.display = 'none' : navMenu.style.display = 'block'
+  }
