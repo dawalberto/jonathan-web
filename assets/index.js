b07752bd796b0edc766279180e5b6686efc98cb5
Alberto Garcia <albertoinformatica97@gmail.com>
2019-09-19 02:16:35 +0200

    Añadida galeria(tengo que comprimir las imagenes para que ocupen menos)

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,8 @@
 function toogleNav() {
-    let navMenu = document.getElementById('navMenu');
-    navMenu.style.display === 'block' ? navMenu.style.display = 'none' : navMenu.style.display = 'block'
-  }
+  let navMenu = document.getElementById('navMenu');
+  navMenu.style.display === 'block' ? navMenu.style.display = 'none' : navMenu.style.display = 'block'
+}
+
+Galleria.loadTheme('librerias/galleria/themes/classic/galleria.classic.min.js')
+
+Galleria.run('.galleria')
